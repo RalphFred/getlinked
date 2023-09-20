b5745ca4a0032b707fd960871c4e2ea2b4dbd67c
rapha~El <111363603+RalphFred@users.noreply.github.com>
2023-09-20 21:47:57 +0100

    Created the constants

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const Questions = [
+  {
+    question: "Can I work on a project I started before the hackathon?",
+    answer: "Yes, you can continue working on a project you started before the hackathon.",
+  },
+  {
+    question: "What happens if I need help during the hackathon?",
+    answer: "During the hackathon, you can typically seek help from mentors, organizers, or fellow participants for assistance and guidance.",
+  },
+  {
+    question: "What happens if I don't have an idea for a project?",
+    answer: "If you don't have an idea for a project during a hackathon, consider joining a team, brainstorming with others, or exploring suggested challenges.",
+  },
+  {
+    question: "Can I join a team or do I have to come with one?",
+    answer: "You can often join a team at a hackathon, especially if you don't have one beforehand. Many hackathons facilitate team formation.",
+  },
+  {
+    question: "What happens after the hackathon ends?",
+    answer: "After the hackathon ends, you present, judges evaluate, and you can continue or explore new opportunities.",
+  },
+]
